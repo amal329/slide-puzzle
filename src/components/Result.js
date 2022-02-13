db1629bda0f67bfbe4fdcb7182ae43aa88915f88
Amal Krishna <AmalKrishna.A@brillio.com>
2022-02-13 14:10:34 +0530

    Added commentary on solving

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 export default function Result({show = false, minutes,seconds}) {
+
+  let msg = "\n\n";
+  
+  if(minutes === 0){
+    if(seconds <= 25){
+      msg += "Dayum, impressed xD";
+    }
+    else{
+      msg += "That was quick :D"
+    }
+  }
+  else if(minutes === 1){
+    msg += "People have solved it in less than 1 minute tho :)";
+  }
+  else{
+    msg += "You can do better hehe";
+  }
+
   if(show){
     return (
         <div className="result">
             You took {minutes>0 ? Math.round(minutes)+" minutes and " : ""} {Math.round(seconds)} seconds!
+            {msg}
         </div>
       )
   }
